e1ec8d94a3a7dd8d076d1bb7e3de712174d810f1
Rodolfo Charles <rosdolfo@gmail.com>
2017-01-17 23:32:53 -0200

    capitulo03-aula05 [v2]

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,7 @@ import {Platform, ionicBootstrap, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {MenuTestPage} from './pages/menu-test/menu-test';
+import {GeneratedTestPage} from './pages/generated-test/generated-test';
 
 
 @Component({
@@ -18,8 +19,9 @@ export class MyApp {
 
     this.pages = [
       {component: HomePage, title: 'Home', icon: 'home'},
-      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'}
-    ]
+      {component: MenuTestPage, title: 'Menu Test', icon: 'menu'},
+      {component: GeneratedTestPage, title: 'Generated Page', icon: 'car'}
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -27,10 +29,17 @@ export class MyApp {
     });
   }
 
-  openPage(page: any) : void {
+  openPage(page: any, menuSide: string) : void {
     this.rootPage = page.component;
-    this.menuCtrl.close();
+    this.menuCtrl.close(menuSide);
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp, [], {
+  menuType: 'push',
+  platforms: {
+    ios: {
+      menuType: 'overlay',
+    }
+  }
+});
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {NavController} from 'ionic-angular';
+import{MenuTestPage} from '../menu-test/menu-test';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -7,5 +8,8 @@ import {NavController} from 'ionic-angular';
 export class HomePage {
   constructor(private navController: NavController) {
 
+  }
+  pushPage() : void {
+    this.navController.push(MenuTestPage);
   }
 }
